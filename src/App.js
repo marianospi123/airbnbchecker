0b26185b8707bbd47f485619ba9c1aa46641be88
marianospi123 <marianospinelli18@gmail.com>
2025-12-12 12:32:11 -0400

    Guaira nuevo

--- src/App.js
+++ src/App.js
@@ -103,9 +103,9 @@ const calendars = [
     airbnbLink: "airbnb.com/h/elparaisoccs",
     esteiLink: "https://surl.li/esteiparaiso",
     airbnb: {
-      pricePerNight: 50,
+      pricePerNight: 55,
       cleaningFee: 50,
-      extraGuestFeePerNight: 5,
+      extraGuestFeePerNight: 7,
       maxGuestsIncluded: 2,
       discountWeek: 0.10,
       discountMonth: 0.20,
@@ -704,6 +704,32 @@ const calendars = [
     platformFeePercentage: 0.15
   },
 },
+{
+  name: "La Guaira II – PH Frente al Mar con Terraza y Acceso Directo a la Playa",
+  estado: "La Guaira",
+  url: "https://www.airbnb.com/calendar/ical/1297827504339510758.ics?s=eedafe1e2ba1db25c7483d102f6c9b52&locale=es-XL",
+  esteiUrl: "https://www.airbnb.com/calendar/ical/1297827504339510758.ics?s=eedafe1e2ba1db25c7483d102f6c9b52&locale=es-XL",
+  capacity: 8,
+  rooms: 3,
+  baths: 4,
+  airbnbLink: "https://airbnb.com/h/laguaira2",
+  esteiLink: "https://estei.app/stay/17653524748602095832/profile",
+  airbnb: {
+    pricePerNight: 145,
+    cleaningFee: 100,
+    extraGuestFeePerNight: 15,
+    maxGuestsIncluded: 4,
+    discountWeek: 0.05,
+    discountMonth: 0.20,
+    platformFeePercentage: 0.1411
+  },
+  estei: {
+    pricePerNight: 250,
+    cleaningFee: 130,
+    platformFeePercentage: 0.15
+  },
+},
+
 {
   name: "Paraíso Familiar en Isla Margarita",
   estado: "Margarita",
